7f126a44d5667fcf237ac3494e16ac6420a062e3
Anirudh Bagalkotker <93025106+AnirudhBagalkotker@users.noreply.github.com>
2025-02-11 22:15:16 +0530

    Updated Header Styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 const Header = () => {
 	return (
-		<div className="h-12 bg-[#1e1e1e] border-b border-[#2d2d2d] flex items-space-between px-4">
+		<div className="h-12 bg-[#1e1e1e] border-b border-[#2d2d2d] flex items-center justify-space-between px-4">
 			<h1 className="text-[#eeeeee] text-xl font-bold select-none">CodeBuddy.ai</h1>
 		</div>
 	);
